4a07d2d1492188ab88aa85615ebfa0c4e3fd2f1b
cjus <Carlos.Justiniano@gmail.com>
2018-09-25 20:36:04 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/sh
 sudo apt-get install python-blinkt
 npm install
-sudo sed -i -e 's/^exit 0/\nexit 0/g' /etc/rc.local
-/usr/local/bin/node /home/pi/raspi-thc/raspi-thc
+sudo sed -i -e 's/^exit 0/cd \/home\/pi\/raspi-thc\nsudo \/usr\/local\/bin\/node raspi-thc \& \nexit 0/g' /etc/rc.local
